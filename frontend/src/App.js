62fd483ba6fe1c3729940faae37cd1cb41c56c4e
Nicholas <nicxzmiller2@gmail.com>
2020-06-30 09:15:22 +0200

    added product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import data from "./data";
+import {BrowserRouter, Link, Route} from 'react-router-dom';
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
 
@@ -14,60 +15,48 @@ function App() {
     };
 
   return (
-      <div className="grid-container">
-          <header className="header">
-              <div className="brand">
-                  <button onClick={openMenu}>
-                      &#9776;
-                  </button>
-                  <a href="index.html">Amazonclone</a>
-              </div>
-              <div className="header-links">
-                  <a href="cart.html">Cart</a>
-                  <a href="signin.html">Sign in</a>
-              </div>
-          </header>
-          <aside className="sidebar">
-              <h3>Shopping Categories</h3>
-              <button className="sidebar-close-button" onClick={closeMenu}>X</button>
-              <ul>
-                  <li>
-                      <a href="index.html">Pants</a>
-                  </li>
-                  <li>
-                      <a href="index.html">Shirts</a>
-                  </li>
-              </ul>
-          </aside>
-
-          <main className="main">
-              <div className="content">
-                  <ul className="products">
-                      {
-                        data.products.map(product =>
-                            <li>
-                          <div className="product">
-                              <img className="product-image" src={product.image} alt="product"/>
-                                  <div className="product-name">
-                                      <a href="product.html">{product.name}</a></div>
-                                  <div className="product-brand">{product.brand}</div>
-                                  <div className="product-price">{product.currency}{product.price}</div>
-                                  <div className="product-rating">{product.rating} stars {product.numReviews}</div>
-                          </div>
+      <BrowserRouter>
+          <div className="grid-container">
+              <header className="header">
+                  <div className="brand">
+                      <button onClick={openMenu}>
+                          &#9776;
+                      </button>
+                      <Link to="/">Amazonclone</Link>
+                  </div>
+                  <div className="header-links">
+                      <a href="cart.html">Cart</a>
+                      <a href="signin.html">Sign in</a>
+                  </div>
+              </header>
+              <aside className="sidebar">
+                  <h3>Shopping Categories</h3>
+                  <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+                  <ul>
+                      <li>
+                          <a href="index.html">Pants</a>
                       </li>
-                        )
-                      }
+                      <li>
+                          <a href="index.html">Shirts</a>
+                      </li>
                   </ul>
-              </div>
-          </main>
+              </aside>
 
-          <footer className="footer">
-              <a href="">Conditions of use</a>
-              <a href="">Privacy Notice</a>
-              <a href="">Interest-based Ads</a>
-              All Rights Reserved
-          </footer>
-      </div>
+              <main className="main">
+                  <div className="content">
+                      <Route path="/product/:id" component={ProductScreen} />
+                      <Route exact={true} path="/" component={HomeScreen} />
+                  </div>
+              </main>
+
+              <footer className="footer">
+                  <a href="">Conditions of use</a>
+                  <a href="">Privacy Notice</a>
+                  <a href="">Interest-based Ads</a>
+                  All Rights Reserved
+              </footer>
+          </div>
+      </BrowserRouter>
   );
 }
 
