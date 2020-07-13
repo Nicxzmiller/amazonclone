d3f926000c77ba03c50b7e12c62f6eb062cbf86b
Nicholas <nicxzmiller2@gmail.com>
2020-07-14 01:56:02 +0200

    place order setup

--- frontend/src/Screens/PaymentScreen.js
+++ frontend/src/Screens/PaymentScreen.js
@@ -12,7 +12,7 @@ function PaymentScreen(props) {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        dispatch(savePayment(paymentMethod));
+        dispatch(savePayment({paymentMethod}));
         props.history.push('placeorder');
     };
 
